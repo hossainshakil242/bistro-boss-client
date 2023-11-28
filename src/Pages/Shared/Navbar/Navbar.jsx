5315c3966897b351eded648362e1d68ada145cf0
hossainshakil242 <hossainshakil242@gmail.com>
2023-11-28 20:05:45 +0600

    make Navbar

---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    const navOptions = <>
+        <li><Link to='/'>HOME</Link></li>
+        <li><Link to='/'>CONTACT US</Link></li>
+        <li><Link to='/'>DASHBOARD</Link></li>
+        <li><Link to='/'>OUR MENU</Link></li>
+        <li><Link to='/'>OUR SHOP</Link></li>
+        <li><Link to='/erro'>SIGN OUT</Link></li>
+    </>
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {navOptions}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">Bistro Boss</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navOptions}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Button</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
