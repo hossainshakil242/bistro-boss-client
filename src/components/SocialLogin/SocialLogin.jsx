7fcd41a5e7864d37cd628c1d96190e8011f3fda8
hossainshakil242 <hossainshakil242@gmail.com>
2024-11-30 21:14:14 +0600

    duplicate email not store in databse

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -3,15 +3,26 @@ import { FaGoogle } from "react-icons/fa";
 import useAuth from "../../hooks/useAuth";
 import { AuthContext } from "../../providers/AuthProvider";
 import { useNavigate } from "react-router-dom";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
 
 const SocialLogin = () => {
     const {googleSignIn} = useAuth();
+    const axiosPublic = useAxiosPublic();
     const navigate = useNavigate();
+
     const handleGoogleSignIn = () => {
         googleSignIn()
         .then(result=> {
             console.log(result);
-            navigate('/');
+            const userInfo = {
+                email: result.user?.email,
+                name: result.user?.displayName,
+            }
+            axiosPublic.post('/users',userInfo)
+            .then(res=>{
+                console.log(res.data);
+                navigate('/');
+            })
         })
         .catch(error=> console.log(error))
     }
