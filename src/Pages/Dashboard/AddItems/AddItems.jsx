913e29ec3ddb1f5f9ad37b4cbbb52dccae07e756
hossainshakil242 <hossainshakil242@gmail.com>
2024-12-05 21:12:09 +0600

    image hosting successfull imgbb

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,11 +1,106 @@
-import React from 'react';
+import React from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { useForm } from "react-hook-form";
+import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddItems = () => {
-    return (
-        <div>
-            <h2 className='text-xl text-center'>Add item Page</h2>
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const axiosPublic = useAxiosPublic();
+
+  const onSubmit = async (data) => {
+    console.log(data,image_hosting_key);
+    //upload image to imgbb and then get an url
+    const imageFile = {image: data.image[0]};
+    const res = await axiosPublic.post(image_hosting_api,imageFile,{
+        headers: {
+            'Content-Type':'multipart/form-data'
+        }
+    });
+    console.log(res.data);
+
+  };
+  return (
+    <div className="px-6 pb-10">
+      <SectionTitle
+        heading={"add items"}
+        subHeading={"whats new"}
+      ></SectionTitle>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label className="form-control w-full my-6">
+            <div className="label">
+              <span className="label-text">Recipe Name*</span>
+            </div>
+            <input
+              type="text"
+              name="name"
+              placeholder="Recipe Name"
+              {...register("name",{required:true})}
+              className="input input-bordered w-full"
+            />
+          </label>
+          <div className="flex gap-6">
+            {/* category */}
+            <label className="form-control w-full my-6">
+              <div className="label">
+                <span className="label-text">Category*</span>
+              </div>
+              <select
+                {...register("category",{required:true})}
+                className="select select-bordered w-full"
+                defaultValue='default'
+              >
+                <option disabled value='default'>
+                  Select a category
+                </option>
+                <option value="salad">Salad</option>
+                <option value="pizza">Pizza</option>
+                <option value="soup">Soup</option>
+                <option value="dessert">Dessert</option>
+                <option value="drinks">Drinks</option>
+              </select>
+            </label>
+            {/* price */}
+            <label className="form-control w-full my-6">
+              <div className="label">
+                <span className="label-text">Price*</span>
+              </div>
+              <input
+                type="number"
+                name="price"
+                placeholder="Price"
+                {...register("price",{required:true})}
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          {/* recipe details */}
+          <label className="form-control">
+            <div className="label">
+              <span className="label-text">Recipe Details</span>
+            </div>
+            <textarea
+              className="textarea textarea-bordered h-24"
+              placeholder="recipe details"
+              {...register('recipe')}
+            ></textarea>
+          </label>
+          <div>
+            {/* file input image */}
+            <input {...register('image',{required:true})} type="file" className="file-input my-4 file-input-bordered w-full max-w-xs" />
+          </div>
+          <button className="btn w-full">
+            Add Item <FaUtensils></FaUtensils>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddItems;
--- src/Pages/Dashboard/_Page_level_security.js
+++ src/Pages/Dashboard/_Page_level_security.js
@@ -1,4 +1,6 @@
 /**
  * 1. Do not show the link to those user who should not see the link.
  * 2. Even if they gets the link, Do not allow them to visit the link.
+ * 3. Do not allow user to access the api, check admin in the server as well (verify)
+ * 4.  
  */
